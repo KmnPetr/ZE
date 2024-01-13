667c8da6e9b22145c36d43abf28408ba94ef7044
KmnPetr <kmn.petrichenko@yandex.ru>
2024-01-13 01:28:04 -0700

    commit

--- WordsManager/src/main/java/com/example/WordsManager/services/BackupWordService.java
+++ WordsManager/src/main/java/com/example/WordsManager/services/BackupWordService.java
@@ -4,7 +4,11 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BackupWordService {
@@ -17,6 +21,51 @@ public class BackupWordService {
 
         File file = new File(filePath+fileName);
 
+        createFile(file);
+
+        List<String> rows = new ArrayList<>();
+        rows.add("Test row 1");
+        rows.add("Test row 2");
+        rows.add("Test row 3");
+        rows.add("Test row 4");
+        rows.add("Test row 5");
+        rows.add("Test row 6");
+        rows.add("Test row 7");
+        rows.add("Test row 8");
+        rows.add("Test row 9");
+        rows.add("Test row 10");
+        rows.add("Test row 11");
+        rows.add("Test row 12");
+        rows.add("Test row 13");
+        rows.add("Test row 14");
+
+        printTextIntoFile(file,rows);
+
+
+    }
+
+    private void printTextIntoFile(File file, List<String> rows) {
+        PrintWriter printWriter;
+        try{
+            printWriter = new PrintWriter(file);
+
+            for (String row:rows) {
+                printWriter.println(row);
+            }
+
+            printWriter.close();
+
+
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * сюда вынесен код по созданию файла
+     */
+    private void createFile(File file) {
         try {
             if (file.createNewFile()) {
                 System.out.println("Файл был успешно создан.");
